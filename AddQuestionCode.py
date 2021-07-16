37281155a1e8e2d8a5724af464dec2c4a8e99133
Hayden <hayd.reid@gmail.com>
2021-07-15 23:14:36 -0400

    Insert New Custom Question ID
    inserting the question ID from the newly created question
    identified the xpath axes of the new question

--- AddQuestionCode.py
+++ AddQuestionCode.py
@@ -46,12 +46,13 @@ totallanguagecount = len(emailinvitationarray[0])
 # print(questionarr[5][2])
 
 
-def letsaddquestions():
+def addcustomquestions():
     questiondrivernames = WebDriverWait(driver, 20).until(
         EC.presence_of_all_elements_located((By.XPATH, "//*[@id='survey_pages_attributes_0_page_questions_attributes_0_title']/following::h4[not(@*)]")))
     # You may have to 'click' the question text area first and then call send_keys for the question text
     for i, excelrowlistobject in enumerate(questionarr):
         if excelrowlistobject[1] is None and excelrowlistobject[2] is not None:
+            # OR condition to check for Y/N toggle for custom question where ID is present
             if excelrowlistobject[2] == "Empty Slot":
                 continue
             else:
@@ -61,7 +62,7 @@ def letsaddquestions():
                 # print("***************************************************")
                 for drivername in questiondrivernames.text:
                     if excelrowlistobject[0] == drivername:
-                        # Click the following Add Question Button
+                        # Click the Add Question Button following the driver name match
                         WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
                             (By.XPATH, f"//h4[not(@*) and contains(text(),{excelrowlistobject[0]})]/following::button[position()=1]"))).click()
                         # AddQuestionTextArea
@@ -70,14 +71,17 @@ def letsaddquestions():
                         # Click Save
                         WebDriverWait(driver, 20).until(
                             EC.element_to_be_clickable((By.XPATH, "//form[@id='add-custom-question-form']/div[@class='modal-footer']/input"))).click()
-
-                        # match excelrowlistobject[2] text to newly added question and obtain the question id, take that value
-                        # and insert it into the questionarr position relative to that text.
-                        # //*[starts-with(@class,'switch-container')]/div[3]/span/strong
-
-# f"//*[starts-with(@class='question-text-area') and contains(text(),{excelrowlistobject[2]})]/following::strong[position()=1]"
-# "//*[starts-with(@class='question-text-area') and contains(text(),'My friends outside work would describe me as having a very positive attitude.')]"
-# need to update the questions ID and add it to the array, that way future iterations are updating those newly added questions
+                        # May need to time.sleep() here and wait for the ID to get pulled
+                        # Grab the Question ID of the new question and add it to questionarr[questionnum][1]
+                        newcustomquestionid = WebDriverWait(driver, 20).until(
+                            EC.element_to_be_clickable((By.XPATH, f"//*[starts-with(@class='question-text-area') and contains(text(),{excelrowlistobject[2]})]/following::strong[position()=2]")))
+                        # Match the text from excelrowlistobject to questionarr[counter][2] (the QIL question text) and then insert the Question ID to prev column
+                        for counter, qilrowlistobject in enumerate(questionarr):
+                            if excelrowlistobject[2] == questionarr[counter][2]:
+                                questionarr[counter][1] = int(newcustomquestionid)
+        print(questionarr[i][1])
+# Example below of matching the newly saved question and grabbing that new Question's ID:
+# "//*[starts-with(@class,'question-text-area') and contains(text(),'My friends outside work would describe me as having a very positive attitude.')]/following::strong[position()=2]"
 
 
 # pass the name of the driver back to the web element object to be found and clicked
